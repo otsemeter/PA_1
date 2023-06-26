c45ac414f194522e9af2090bdf743fa7ab84f7cb
owen <owensemeter@gmail.com>
2023-06-25 23:12:44 -0400

    now with comments

--- sequence.c
+++ sequence.c
@@ -1,10 +1,15 @@
+//preprocessor directives
 #include "sequence.h"
 #include<stdio.h>
 #include<stdlib.h>
 
+//helper function declarations
 int log_2(int n);
-long *Generate_2p3q_Seq(int n, int *seq_size);
 
+/* calculate log base 2 of n by 
+    continuously dividing n by 2 
+    until n is lessthan or equal to 1
+*/
 int log_2(int n)
 {
     int logn = 0;
@@ -18,6 +23,36 @@ int log_2(int n)
     return logn;
 }
 
+/* Generate_2p3q_Seq
+   ~~~~~~~~~~~~~~~~~
+   inputs: int n, int * seq_size
+   
+   n: size of array sequence is being generated for
+   seq_size: pointer to location where sequence size will be stored
+   
+   instance variables: int index_3, int index_2, int index_curr, long val_3, long val_2, int logn, long * seq
+   
+   index_3: index of the most recent base 3 value calculated
+   index_2: index of the most recent base 2 value calculated
+   index_curr: index of the current sequence value
+   val_3: most recent base 3 value calculated
+   val_2: most recent base 2 value calculated
+   logn: log_2 of n
+   seq: array to hold the sequence values
+   
+   functionality:
+   
+   preliminarily set seq_size to log_2 n squared to allocated sufficient memory
+   initialize index 0 of seq to 1
+   do-while loop, itterating as long as the current seq index's value is less than n, the array size
+   first, itterate the current index to 1 because 0 was initialized
+   set val_3 to the most recent base 3 index * 3
+   set val_2 to the most recent base 2 index * 2
+   if val_3 is less than val_2, set the current index of seq to val_3, otherwise set it to val_2
+   itterate index_3 if val_3 was less than val_2, and do the inverse for index_2
+   exit loop
+   set seq_size to the last index, index_curr and return the sequence
+   */
 long *Generate_2p3q_Seq(int n, int *seq_size)
 {
     int index_3 = 0;
